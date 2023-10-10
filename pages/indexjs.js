1a6fce21a5d5de6fd91432ee9f88fb837fd3c427
Joseph Kwame Agyei <jagyei339@gmail.com>
2023-10-10 12:01:30 +0000

    first push

---
+++ pages/indexjs.js
@@ -0,0 +1,12 @@
+let buttonUp=document.querySelector("#bup");
+buttonUp.style.display="none";
+buttonUp.addEventListener("scroll",function(){
+    // if(){
+
+    // }else{
+
+    // }
+});
+buttonUp.addEventListener("click",function(){
+  
+});
